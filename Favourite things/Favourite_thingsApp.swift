1232ebf943b32ea9ab10a3273aa6195796f31971
yining wang <ywang@lcs.on.ca>
2021-02-04 19:29:13 -0500

    Initial Commit

---
+++ Favourite things/Favourite_thingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite_thingsApp.swift
+//  Favourite things
+//
+//  Created by Yining Wang on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct Favourite_thingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
