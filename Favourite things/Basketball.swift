5c7ecadf91661ecaad6fbb77ce3164bff5653a76
yining wang <ywang@lcs.on.ca>
2021-02-07 17:32:25 -0500

    Basketball part is completed

--- Favourite things/Basketball.swift
+++ Favourite things/Basketball.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct Basketball: View {
     var body: some View {
         VStack {
+            Image ("Basketball 1")
+                .resizable()
+                .padding()
+                .scaledToFit()
             
+            HStack{
+                Image ("Basketball 2")
+                    .resizable()
+                    .padding(.leading)
+                    .scaledToFit()
+                
+                Text("Baketball is my favorite sport. I really like to play basketball with my friends and watch the NBA games. Basketball is of great significance to promote the enhancement of my physical fitness and the cultivation of healthy psychology ")
+                    .multilineTextAlignment(.leading)
+                    .padding(.trailing)
+                
             }
             
-            
+            Image ("Basketball 3")
+                .resizable()
+                .padding(.horizontal)
+                .scaledToFit()
+        }
+        .navigationTitle("Basketball")
     }
 }
 
